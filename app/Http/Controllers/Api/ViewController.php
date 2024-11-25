ca5421da3656277026439027932c7146b5a88a0e
ntnhat123 <nhatnguyen.tk2@gmail.com>
2024-11-25 15:12:26 +0700

    getimage chapter

--- app/Http/Controllers/Api/ViewController.php
+++ app/Http/Controllers/Api/ViewController.php
@@ -17,7 +17,7 @@ class ViewController extends Controller
 {
     public function startView(Request $request)
     {
-        
+
         // Validate các dữ liệu đầu vào
         $validator = Validator::make($request->all(), [
             'user_id' => 'required',
@@ -54,7 +54,7 @@ class ViewController extends Controller
         $validator = Validator::make($request->all(), [
             'user_id' => 'required',
         ]);
-    
+
         if ($validator->fails()) {
             return response()->json([
                 'status' => false,
@@ -62,35 +62,36 @@ class ViewController extends Controller
                 'errors' => $validator->errors(),
             ], 400);
         }
-    
+
         $userId = $request->user_id;
-    
+
         // Lấy 5 lần xem gần nhất của user theo `user_id`
         $lastViews = View::where('user_id', $userId)
             ->orderBy('id', 'desc')
             ->take(5)
             ->get();
-    
+
         $data = [];
         foreach ($lastViews as $view) {
             // Lấy thông tin bài học hiện tại
             $lesstionChapter = LesstionChapter::with(['chapterSubject'])
                 ->where('id_lesstion_chapter', $view->id_view_query)
                 ->first();
-    
+
             if ($lesstionChapter) {
                 // Lấy bài học tiếp theo
                 $nextLesson = LesstionChapter::where('chapter_subject_id', $lesstionChapter->chapter_subject_id)
                     ->where('number_lesstion_chapter', '>', $lesstionChapter->number_lesstion_chapter)
                     ->orderBy('number_lesstion_chapter', 'asc')
                     ->first();
-    
+
                 // Kiểm tra nếu tồn tại bài học tiếp theo
                 if ($nextLesson) {
                     $chapter = $lesstionChapter->chapterSubject;
-    
+                    $chapter->chapter_image = asset('images/' . $chapter->chapter_image);
+
                     // Gắn bài học tiếp theo vào chương
-    
+
                     // Chỉ thêm mục này vào data nếu có next_lesson
                     $data[] = [
                         'chapter' => $chapter,
@@ -110,7 +111,7 @@ class ViewController extends Controller
     //     $validator = Validator::make($request->all(), [
     //         'user_id' => 'required',
     //     ]);
-    
+
     //     if ($validator->fails()) {
     //         return response()->json([
     //             'status' => false,
@@ -118,35 +119,35 @@ class ViewController extends Controller
     //             'errors' => $validator->errors(),
     //         ], 400);
     //     }
-    
+
     //     $userId = $request->user_id;
-    
+
     //     // Lấy 5 lần xem gần nhất của user theo `user_id`
     //     $lastViews = View::where('user_id', $userId)
     //         ->orderBy('id', 'desc')
     //         ->take(5)
     //         ->get();
-    
+
     //     $data = [];
     //     foreach ($lastViews as $view) {
     //         // Lấy thông tin bài học hiện tại
     //         $lesstionChapter = LesstionChapter::with(['chapterSubject'])
     //             ->where('id_lesstion_chapter', $view->id_view_query)
     //             ->first();
-    
+
     //         if ($lesstionChapter) {
     //             // Lấy bài học tiếp theo
     //             $nextLesson = LesstionChapter::where('chapter_subject_id', $lesstionChapter->chapter_subject_id)
     //                 ->where('number_lesstion_chapter', '>', $lesstionChapter->number_lesstion_chapter)
     //                 ->orderBy('number_lesstion_chapter', 'asc')
     //                 ->first();
-    
+
     //             // Kiểm tra nếu tồn tại bài học tiếp theo
     //             if ($nextLesson) {
     //                 $chapter = $lesstionChapter->chapterSubject;
-    
+
     //                 // Gắn bài học tiếp theo vào chương
-    
+
     //                 // Chỉ thêm mục này vào data nếu có next_lesson
     //                 $data[] = [
     //                     'chapter' => $chapter,
